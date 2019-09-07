b6293716fb6da9084017d2bb03cc69ae56c96e07
Syed Muhammad Taha <taha@loopsdigital.com>
2019-09-07 17:32:22 +0500

    feat: add signin page

---
+++ src/pages/auth/signin-page/signin-page.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+import { el } from '../../../shared/el';
+import AuthTitle from '../auth-title';
+import AuthForm from '../auth-form';
+
+export interface SigninPageProps {}
+
+const SigninPage: React.SFC<SigninPageProps> = () => {
+  return el(
+    'div',
+    { className: 'p-5 card-body' },
+    AuthTitle({
+      title: 'Signin'
+    }),
+    AuthForm({
+      labelGroupsProps: [
+        {
+          input: {
+            type: 'email',
+            id: 'email',
+            className: 'form-control',
+            placeholder: 'Email',
+            required: true,
+            autofocus: true
+          },
+          label: { text: 'Email' }
+        },
+        {
+          input: {
+            type: 'password',
+            id: 'password',
+            className: 'form-control',
+            placeholder: 'Password',
+            required: true,
+            autofocus: true
+          },
+          label: { text: 'Password' }
+        }
+      ],
+      buttonText: 'Submit'
+    })
+  );
+};
+
+export default SigninPage;
--- src/pages/auth/signup-page/signup-page.tsx
+++ src/pages/auth/signup-page/signup-page.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import AuthCard from '../auth-card';
 import AuthTitle from '../auth-title';
 import AuthForm from '../auth-form';
 
@@ -17,49 +16,37 @@ class SignupPage extends React.Component<SignupPageProps, SignupPageState> {
   render() {
     return el(
       'div',
-      { className: 'container' },
-      el(
-        'div',
-        { className: 'row' },
-        el(
-          'div',
-          { className: 'col-sm-9 col-md-7 col-lg-5 mx-auto' },
-          el(AuthCard, {
-            body: el(
-              'div',
-              { className: 'p-5 card-body' },
-              el(AuthTitle, { title: 'Signup' }),
-              el(AuthForm, {
-                labelGroupsProps: [
-                  {
-                    input: {
-                      type: 'email',
-                      id: 'email',
-                      className: 'form-control',
-                      placeholder: 'Email',
-                      required: true,
-                      autofocus: true
-                    },
-                    label: { text: 'Email' }
-                  },
-                  {
-                    input: {
-                      type: 'password',
-                      id: 'password',
-                      className: 'form-control',
-                      placeholder: 'Password',
-                      required: true,
-                      autofocus: true
-                    },
-                    label: { text: 'Password' }
-                  }
-                ],
-                buttonText: 'Submit'
-              })
-            )
-          })
-        )
-      )
+      { className: 'p-5 card-body' },
+      AuthTitle({
+        title: 'Signup'
+      }),
+      AuthForm({
+        labelGroupsProps: [
+          {
+            input: {
+              type: 'email',
+              id: 'email',
+              className: 'form-control',
+              placeholder: 'Email',
+              required: true,
+              autofocus: true
+            },
+            label: { text: 'Email' }
+          },
+          {
+            input: {
+              type: 'password',
+              id: 'password',
+              className: 'form-control',
+              placeholder: 'Password',
+              required: true,
+              autofocus: true
+            },
+            label: { text: 'Password' }
+          }
+        ],
+        buttonText: 'Submit'
+      })
     );
   }
 }
